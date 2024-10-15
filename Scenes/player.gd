a1e6d1d69b771337a7ccce386935a9cf83d59125
Natan henrique M. R. Baptista <espiffotheraccoon@gmail.com>
2024-10-15 17:11:22 -0300

    Initial Commit
    V1.0.0

---
+++ Scenes/player.gd
@@ -0,0 +1,23 @@
+extends StaticBody2D
+
+var win_height : int
+var p_height : int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	win_height = get_viewport_rect().size.y
+	p_height = $ColorRect.get_size().y
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#Controls of the Player
+	if Input.is_action_pressed("ui_up"):
+		#Goes Up
+		position.y -= get_parent().PADDLE_SPEED * delta
+	elif Input.is_action_pressed("ui_down"):
+		#Goes Down
+		position.y += get_parent().PADDLE_SPEED * delta
+	
+	#Limit paddle movement to window
+	position.y = clamp(position.y, p_height / 2, win_height - p_height / 2)	
+	
